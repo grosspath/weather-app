4846f1f47c06d57d6e2822bab5461df99989db53
Jerol Graves <jerolgraves@gmail.com>
2017-10-09 10:38:24 -0400

    staging for deployment

--- js/index.js
+++ js/index.js
@@ -2,13 +2,14 @@ import { BrowserRouter } from 'react-router-dom';
 import { render } from 'react-dom';
 import React from 'react';
 import Main from './routes/main';
+import ViewHome from './views/view-home'
 
 //The application starts here, establishes the browser router and calls main
 //as the first component
 render((
-  <BrowserRouter>
-    <Main />
-  </BrowserRouter>
+  // <BrowserRouter>
+    <ViewHome />
+  // </BrowserRouter>
   ),
   document.getElementById('root')
 );
